78c52adbe98732ae2c27c84c191234fd9522a756
Arjun Raj <arjunrajvilakkat@outlook.com>
2025-08-12 17:07:36 +0530

    some changes

--- app.js
+++ app.js
@@ -2,24 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("first middleware");
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("second middleware");
-  res.send("<h1>Home page</h1>");
-});
-
-// app.use("/users", (req, res, next) => {
-//   console.log("Users routing middleware");
-//   res.send("<h2>Users Page</h2>");
+// app.use((req, res, next) => {
+//   console.log("first middleware");
+//   next();
 // });
 
-// app.use("/", (req, res, next) => {
-//   console.log("Home Routing Middleware");
-//   res.send("<h1>Home Page</h1>");
+// app.use((req, res, next) => {
+//   console.log("second middleware");
+//   res.send("<h1>Home page</h1>");
 // });
+
+app.use("/users", (req, res, next) => {
+  console.log("Users routing middleware");
+  res.send("<h2>Users Page</h2>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("Home Routing Middleware");
+  res.send("<h1>Home Page</h1>");
+});
 
 app.listen(3000);
