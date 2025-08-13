a2bb6f2ac7ea675996be57d67b8e98ca0cc36060
Arjun Raj <arjunrajvilakkat@outlook.com>
2025-08-13 11:34:51 +0530

    serving html files as response

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 
 const adminRouter = require("./routes/admin");
@@ -13,7 +15,8 @@ app.use(userRouter.router);
 
 // 404 error page
 app.use((req, res, next) => {
-  res.status(404).send("<h1>404. Not Found !");
+  res.status(404);
+  res.sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,16 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/add-user", (req, res, next) => {
-  res.send(`<html>
-    <head><title>Post Example</title></head>
-    <body>
-    <form action="/admin/save-user" method="POST">
-    <div>
-    <input type="text" name="username" />
-    </div>
-    <button type="submit">Add User</button>
-    </form></body></html>`);
+  // path module is required to get the absolute path according to the OS.
+  // __dirname is used to get current directory, like where the script executed
+  // ../ is go to go up one level in directory
+  // views is our folder and last one is our file name
+  // we do it like this because different OS have different way of accessing dirs eg: windows - c:\users\html linux- /usr/bin/lib... etc
+  res.sendFile(path.join(__dirname, "../", "views", "add-user.html"));
 });
 
 router.post("/save-user", (req, res, next) => {
