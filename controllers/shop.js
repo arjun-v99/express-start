de20f3b48d690ede10a54495b3758e42a4aa91b8
Arjun Raj <arjunrajvilakkat@outlook.com>
2025-09-12 22:28:05 +0530

    view and delete cart items, add order

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,5 @@
 // product model
 const Product = require("../models/product");
-// const Cart = require("../models/cart");
 
 exports.listProducts = (req, res, next) => {
   Product.fetchAll()
@@ -26,24 +25,18 @@ exports.getHome = (req, res, next) => {
     .catch((err) => console.error(err));
 };
 
-// exports.getCart = (req, res, next) => {
-//   req.user
-//     .getCart()
-//     .then((cart) => {
-//       return cart
-//         .getProducts()
-//         .then((products) => {
-//           res.render("shop/cart", {
-//             path: "/cart",
-//             pageTitle: "Your Cart",
-//             products: products,
-//           });
-//         })
-//         .catch((err) => console.error(err));
-//     })
-
-//     .catch((err) => console.error(err));
-// };
+exports.getCart = (req, res, next) => {
+  req.user
+    .getCart()
+    .then((products) => {
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products: products,
+      });
+    })
+    .catch((err) => console.error(err));
+};
 
 exports.addToCart = (req, res, next) => {
   const productId = req.body.productId;
@@ -51,43 +44,32 @@ exports.addToCart = (req, res, next) => {
     .then((product) => {
       return req.user.addToCart(product);
     })
-    .then((result) => res.redirect("/"))
+    .then((result) => res.redirect("/cart"))
     .catch((err) => console.error(err));
 };
 
-// exports.postCartDeleteProduct = (req, res, next) => {
-//   const prodId = req.body.productId;
-//   req.user
-//     .getCart()
-//     .then((cart) => {
-//       return cart.getProducts({ where: { id: prodId } });
-//     })
-//     .then((products) => {
-//       const product = products[0];
-//       return product.cartItem.destroy();
-//     })
-//     .then((result) => {
-//       res.redirect("/cart");
-//     })
-//     .catch((err) => console.error(err));
-// };
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  req.user
+    .deleteItemFromCart(prodId)
+    .then((products) => {
+      res.redirect("/cart");
+    })
+    .catch((err) => console.error(err));
+};
 
-// exports.getOrders = (req, res, next) => {
-//   req.user
-//     //we are including products because fetching orders does not fetch products by default.
-//     // so we have to explicitly tell sequelize to get products also. \
-//     // this only works if we have defined th relation ofcourse
-//     .getOrders({ include: ["products"] })
-//     .then((orders) => {
-//       console.log(orders);
-//       res.render("shop/orders", {
-//         pageTitle: "Orders",
-//         path: "/orders",
-//         orders: orders,
-//       });
-//     })
-//     .catch((err) => console.log(err));
-// };
+exports.getOrders = (req, res, next) => {
+  req.user
+    .getOrders()
+    .then((orders) => {
+      res.render("shop/orders", {
+        pageTitle: "Orders",
+        path: "/orders",
+        orders: orders,
+      });
+    })
+    .catch((err) => console.log(err));
+};
 
 exports.getProductDetail = (req, res, next) => {
   const prodId = req.params.productId;
@@ -101,48 +83,13 @@ exports.getProductDetail = (req, res, next) => {
       });
     })
     .catch((err) => console.error(err));
-
-  // Product.findByPk(prodId)
-  //   .then((product) => {
-  //     res.render("shop/product-detail", {
-  //       product: product,
-  //       pageTitle: product.title + " Detail",
-  //       path: "/products",
-  //     });
-  //   })
-  //   .catch((err) => console.error(err));
 };
 
-// exports.createOrder = (req, res, next) => {
-//   let fetchedCart;
-//   req.user
-//     .getCart()
-//     .then((cart) => {
-//       fetchedCart = cart;
-//       return cart.getProducts();
-//     })
-//     .then((products) => {
-//       // creating order then adding products
-//       return req.user
-//         .createOrder()
-//         .then((order) => {
-//           // since different products have different quantites we use a different approach than we used in the cart
-//           return order.addProducts(
-//             // map is used through all elements do operations in the element and return a new array with the modifications
-//             products.map((product) => {
-//               // you have to
-//               product.orderItem = { quantity: product.cartItem.quantity };
-//               return product;
-//             })
-//           );
-//         })
-//         .catch((err) => console.error(err));
-//     })
-//     .then((result) => {
-//       return fetchedCart.setProducts(null);
-//     })
-//     .then((result) => {
-//       res.redirect("/orders");
-//     })
-//     .catch((err) => console.error(err));
-// };
+exports.createOrder = (req, res, next) => {
+  req.user
+    .addOrder()
+    .then((result) => {
+      res.redirect("/orders");
+    })
+    .catch((err) => console.error(err));
+};
