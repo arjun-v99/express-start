9e37b0406637be9f3989e786920c8a446bf1207d
Arjun Raj <arjunrajvilakkat@outlook.com>
2025-09-23 14:24:52 +0530

    reset password complete

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,28 @@
+const crypto = require("crypto");
+
 const bcrypt = require("bcryptjs");
 const { MailtrapClient } = require("mailtrap");
 
 const TOKEN = process.env.MAILTRAP_TOKEN;
 
 const User = require("../models/user");
+const user = require("../models/user");
 
 const client = new MailtrapClient({
   token: TOKEN,
   sandbox: true,
   testInboxId: 4047137,
 });
+
+const SENDER = {
+  email: "hello@example.com",
+  name: "Mailtrap Test",
+};
+const RECIPIENTS = [
+  {
+    email: process.env.MAILTRAP_RECIPIENT_MAIL,
+  },
+];
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
@@ -100,21 +113,11 @@ exports.signUp = (req, res, next) => {
         .then(() => {
           res.redirect("/login");
 
-          const sender = {
-            email: "hello@example.com",
-            name: "Mailtrap Test",
-          };
-          const recipients = [
-            {
-              email: "arjun46rv@gmail.com",
-            },
-          ];
-
           // Send Email
           return client
             .send({
-              from: sender,
-              to: recipients,
+              from: SENDER,
+              to: RECIPIENTS,
               subject: "Welcom to Express Start",
               text: "Enjoy our services. Don't hesitate to keep in touch with us for your feedback",
               category: "Integration Test",
@@ -127,4 +130,123 @@ exports.signUp = (req, res, next) => {
     })
 
     .catch((err) => console.error(err));
+};
+
+exports.getResetPwd = (req, res, next) => {
+  let errorMessage = req.flash("error");
+  if (errorMessage.length > 0) {
+    errorMessage = errorMessage[0];
+  } else {
+    errorMessage = null;
+  }
+  let message = req.flash("message");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/reset-pwd", {
+    path: "/reset-password",
+    pageTitle: "Reset Password",
+    errorMsg: errorMessage,
+    message: message,
+  });
+};
+
+exports.postResetPwd = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect("/");
+    }
+    const email = req.body.email;
+    const token = buffer.toString("hex");
+    User.findOne({ email: email })
+      .then((user) => {
+        if (!user) {
+          req.flash("error", "User not found");
+          return res.redirect("/login");
+        }
+        user.resetPwdToken = token;
+        user.resetTokenExpiry = Date.now() + 7200000; //in miiliseconds
+        return user.save();
+      })
+      .then((result) => {
+        return client.send({
+          from: SENDER,
+          to: RECIPIENTS,
+          subject: "Password Reset",
+          html: `<body>
+              <p>You have requested for a password reset.Please click on the link below to reset your passowrd or copy and paste it into your browser.</p> 
+              <div>Link: ${process.env.DOMAIN}reset-password/${token}</div>
+              <br>
+              <div>The link will be valid for 2 Hours</div>
+              </body>
+              `,
+          category: "Integration Test",
+        });
+      })
+      .then(() => {
+        req.flash(
+          "message",
+          "A password reset link has been sent to your registered email address"
+        );
+        res.redirect("/reset-password");
+      })
+      .catch((err) => console.error(err));
+  });
+};
+
+exports.setNewPassword = (req, res, next) => {
+  User.findOne({
+    resetPwdToken: req.params.token,
+    resetTokenExpiry: { $gt: Date.now() },
+  })
+    .then((user) => {
+      let errorMessage = req.flash("error");
+      if (errorMessage.length > 0) {
+        errorMessage = errorMessage[0];
+      } else {
+        errorMessage = null;
+      }
+      res.render("auth/new-pwd", {
+        path: "/reset-password",
+        pageTitle: "Set new password",
+        errorMsg: errorMessage,
+        userId: user._id.toString(),
+        pwdToken: user.resetPwdToken,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+exports.postUpdateNewPwd = (req, res, next) => {
+  const password = req.body.password;
+  const pwdToken = req.body.pwdToken;
+  const userId = req.body.userId;
+  let foundUser;
+
+  User.findOne({
+    resetPwdToken: pwdToken,
+    resetTokenExpiry: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((user) => {
+      foundUser = user;
+      return bcrypt.hash(password, 12);
+    })
+    .then((hashedPwd) => {
+      foundUser.password = hashedPwd;
+      foundUser.resetPwdToken = undefined;
+      foundUser.resetTokenExpiry = undefined;
+      return foundUser.save();
+    })
+    .then(() => {
+      res.redirect("/login");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 };
